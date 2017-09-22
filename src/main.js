8a642a440cb7f30a9a8d91fa86d4716128cfa942
Kostya Danko <Danko.Kostiantyn@bcgdv.com>
2017-09-22 14:56:40 -0700

    initial commit for Vue.js app #2

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import './style.scss';
+import Vue from 'vue';
+
+import MovieFilter from './components/MovieFilter.vue';
+import MovieList from './components/MovieList.vue';
+
+new Vue({
+    el: "#app",
+    data: {
+        genre: [],
+        time: []
+    },
+    methods: {
+        checkFilter(category, title, checked) {
+            if (checked) {
+                this[category].push(title);
+            } else {
+                let index = this[category].indexOf(title);
+                if (index > -1) {
+                    this[category].splice(index, 1);
+                }
+            }
+        }
+    },
+    components: {
+        MovieList,
+        MovieFilter
+    }
+});
