8cdcd5a64645acf2cc990c30af60f0f21c3db2d0
Kostya Danko <Danko.Kostiantyn@bcgdv.com>
2017-09-26 11:29:36 -0700

    added day selectir control

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="overview">
+        <day-select v-bind:selected="day"></day-select>
         <div class="main">
             <movie-list v-bind:genre="genre" v-bind:time="time" v-bind:movies="movies" v-bind:day="day"></movie-list>
             <movie-filter></movie-filter>
@@ -10,12 +11,14 @@
 <script>
     import MovieFilter from './MovieFilter.vue';
     import MovieList from './MovieList.vue';
+    import DaySelect from './DaySelect.vue';
     
     export default {
         props: [ 'genre', 'time', 'movies', 'day' ],
         components: {
             MovieList,
-            MovieFilter
+            MovieFilter,
+            DaySelect
         }
     }
 
